296dc33c30b6a3c3bac55094e19afdec51c13a82
LeeChung <84800904+steadypractice@users.noreply.github.com>
2022-02-15 15:45:04 +0900

    코드 주석 작성

--- 2022.02/_220215/ChungLee/BOJ_1769_G5_암호만들기.java
+++ 2022.02/_220215/ChungLee/BOJ_1769_G5_암호만들기.java
@@ -1,5 +1,10 @@
 package _220215.ChungLee;
 
+/**
+ * @author LeeChung
+ * @problem 암호 만들기
+ * @link https://www.acmicpc.net/problem/1759
+ */
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -9,48 +14,57 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_1769_G5_암호만들기 {
-	static int strCnt = 0;
+	static int L = 0;
 	static String[] save = null;
-	static int apbCnt = 0;
-	static String[] alpa = null;
+	static int C = 0;
+	static String[] totalAlphabet = null;
+	static StringBuilder sb = new StringBuilder();
 
-	static void a(int arrCnt, int ptApb) {
-		int isMoTrue = 0;
-		int isJaTrue = 0;
-		if(ptApb > apbCnt || (ptApb == apbCnt && arrCnt < strCnt)) {
+	static void DFS_BackTracking(int arrCnt, int ptApb) {
+		if(ptApb > C || (ptApb == C && arrCnt < L)) {
 			return;
 		}
-		else if(arrCnt == strCnt) {
+		else if (arrCnt == L) {
+			// 모음 수 카운트
+			boolean isMoTrue = false;
+			int isJaTrue = 0;
+
+			// 모음과 자음의 수를 카운트
 			for (int i = 0; i < save.length; i++) {
 				if (save[i].equals("a") || save[i].equals("e") || save[i].equals("i") || save[i].equals("o")
 						|| save[i].equals("u")) {
-					isMoTrue = 1;
-				}
-				else {
+					isMoTrue = true;
+				} else {
 					isJaTrue++;
 				}
 			}
-			if(isMoTrue == 1 && isJaTrue > 1) {
-				for (int i = 0; i < save.length; i++) 
-					System.out.print(save[i]);
-				System.out.println();
-				return;
+
+			// 모음이 최소 1개이거나 자음이 2개 이상일 때 출력
+			if (isMoTrue && (isJaTrue > 1)) {
+				for (int i = 0; i < save.length; i++)
+					sb.append(save[i]);
+				sb.append("\n");
 			}
 			return;
 		}
 
 		int last = 0;
+		
+		//빈칸을 찾아 값을 넣는 것
 		for (int i = 0; i < save.length; i++) {
-			if (save[i].equals("") || save[i].equals(null)) {
-				save[i] = alpa[ptApb];
+			if (save[i] == null || save[i].equals("")) {
+				save[i] = totalAlphabet[ptApb];
+				// 마지막으로 넣었던 값의 위치 저장
 				last = i;
 				break;
 			}
 		}
 
-		a(arrCnt + 1, ptApb+1 );
+		// 이진 탐색
+		DFS_BackTracking(arrCnt + 1, ptApb + 1);
+		// 넣어주었던 값을 삭제
 		save[last] = "";
-		a(arrCnt, ptApb+1 );
+		DFS_BackTracking(arrCnt, ptApb + 1);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -58,15 +72,23 @@ public class BOJ_1769_G5_암호만들기 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		st = new StringTokenizer(br.readLine());
-		strCnt = Integer.parseInt(st.nextToken());
-		apbCnt = Integer.parseInt(st.nextToken());
-		alpa = br.readLine().split(" ");
-		save = new String[strCnt];
-		for (int i = 0; i < save.length; i++) {
-			save[i] = "";
-		}
-		Arrays.sort(alpa);
 
-		a(0, 0);
+		// ex) 4 입력
+		L = Integer.parseInt(st.nextToken());
+		// ex) 6 입력
+		C = Integer.parseInt(st.nextToken());
+
+		// ex) a t c i s w 입력
+		totalAlphabet = br.readLine().split(" ");
+
+		// 가능한 암호를 저장할 배열 초기화
+		save = new String[L];
+
+		// 사전순이기 때문에 문자열 정렬
+		Arrays.sort(totalAlphabet);
+
+		DFS_BackTracking(0, 0);
+		bw.write(sb.toString());
+		bw.flush();
 	}
 }
