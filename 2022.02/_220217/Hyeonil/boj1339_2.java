5331b5767bf031bd2d2e7243119b058f23bab538
이현일 <gusdlf1836@naver.com>
2022-02-17 12:59:44 +0900

    Hi

--- 2022.02/_220217/Hyeonil/boj1339_2.java
+++ 2022.02/_220217/Hyeonil/boj1339_2.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-
+//11536kb	88ms
 public class boj1339_2 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
