fd9fda3d3614c6e920c1c264d6960219f08ace99
이청 <s2econd.blue@gmail.com>
2022-10-10 18:55:01 +0900

    10-10 알고리즘 업로드

---
+++ 2022.10/LC/PG_불량사용자.java
@@ -0,0 +1,101 @@
+package LC;
+
+import java.util.*;
+
+import java.io.*;
+
+class PG_불량사용자 {
+	static class Solution {
+		static String[] su, sb;
+		static int answer = 0, umaxSize, bmaxSize;
+		static boolean[] isVisit;
+		static int[] saver;
+		static Set<String> s = new HashSet<>();
+		static StringBuilder stb;
+
+		// 현재 응모자 아이디 순번, 불량 아이디 순번
+		static void dfs(int crntPos, int cnt) {
+
+			// 모든 불량 이용자와 비교되었을 시
+			if (cnt == bmaxSize) {
+				// 저장된 순서를 정렬
+				Arrays.sort(saver);
+				stb = new StringBuilder();
+
+				// 정렬된 값을 문자열로 치환
+				for (int i = 0; i < bmaxSize; i++) {
+					stb.append(saver[i]);
+				}
+				// set에 저장되어있는지 확인 후 없다면 추가 및 정답 추가
+				if (!s.contains(stb.toString())) {
+					s.add(stb.toString());
+					answer++;
+				}
+				// 마지막이기 때문에 리턴
+				return;
+			}
+			// 모든 유저를 순회했을 시 리턴
+			if (crntPos == umaxSize)
+				return;
+
+			// 현재 순번 유저가 불량 사용자인 경우
+			for (int i = 0; i < bmaxSize; i++) {
+				// 해당 불량 아이디는 이미 존재한 경우 pass
+				if (isVisit[i])
+					continue;
+				// 해당 유저와 불량 아이디의 길이가 다른 경우 pass
+				else if (su[crntPos].length() != sb[i].length())
+					continue;
+
+				boolean isNotSame = false;
+				// 각 자리를 비교
+				for (int j = 0; j < sb[i].length(); j++) {
+					// 자리가 다른 경우 탐색
+					if (su[crntPos].charAt(j) != sb[i].charAt(j)) {
+						// * 자리일 경우 pass
+						if (sb[i].charAt(j) == '*')
+							continue;
+						// 그 외에는 다른 것으로 판단
+						isNotSame = true;
+						break;
+					}
+				}
+				// 같이 않은 경우 다른 불량 사용자와 비교
+				if (isNotSame)
+					continue;
+				// 적합한 불량 사용자 발견 시
+				else {
+					// 해당 불량 사용자 사용 처리
+					isVisit[i] = true;
+					// 응모자 아이디 순번 저장
+					saver[cnt] = crntPos;
+					dfs(crntPos + 1, cnt + 1);
+					isVisit[i] = false;
+				}
+			}
+
+			// 현재 순번 유저가 불량 사용자가 아닌 경우
+			dfs(crntPos + 1, cnt);
+		}
+
+		public int solution(String[] user_id, String[] banned_id) {
+			// 입력받은 값 스태틱에 저장
+			su = user_id;
+			sb = banned_id;
+			// 각 배열 크기 저장
+			umaxSize = user_id.length;
+			bmaxSize = banned_id.length;
+			// 방문 배열 저장
+			isVisit = new boolean[bmaxSize];
+			// 순서 저장
+			saver = new int[bmaxSize];
+			dfs(0, 0);
+			return answer;
+		}
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+//		System.out.println(Arrays.toString(s.Solution(5, new int[] { 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 })));
+	}
+}
---
+++ 2022.10/LC/PG_양궁대회.java
@@ -0,0 +1,86 @@
+package LC;
+
+import java.util.*;
+
+// 어피치보다 많이 쏴야 고득점.
+// 각 포인트별로 어피치보다 많이 맞춰야 득점 가능.
+// 어피치가 맞추지 못한 곳은 1발만 맞추면 됨.
+public class PG_양궁대회 {
+	static class Solution {
+		// 가장 큰 점수 차이
+		static int maxNum = 0;
+		// 최대, 라이언, 어치피 각각 포인트 별 맞춘 화살 갯수 기록
+		static int[] maxEachArrow = new int[11], lionEachArrow = new int[11], peachEachArrow = new int[11];
+
+		static public void dfs(int crntPos, int remainArrow) {
+			// 마지막 0점 자리 순서인 경우
+			if (crntPos == 10) {
+				// 라이언의 남은 화살을 모두 0번 자리에 입력
+				lionEachArrow[10] = remainArrow;
+
+				// 두 선수 점수 계산 및 비교
+				int lion = 0, peach = 0;
+				for (int i = 0; i < 11; i++) {
+					// 라이언 득점
+					if (lionEachArrow[i] > peachEachArrow[i]) {
+						lion += 10 - i;
+					}
+					// 어피치 득점
+					else if (peachEachArrow[i] != 0) {
+						peach += 10 - i;
+					}
+				}
+
+				// 두 점수 간 차이가 기존 최대 점수 차이를 넘길 경우 값과 화살 맞춘 기록 갱신
+				if (lion - peach > maxNum) {
+					maxNum = lion - peach;
+					maxEachArrow = Arrays.copyOf(lionEachArrow, lionEachArrow.length);
+				}
+
+				// 점수 차가 기존과 같은 경우
+				else if (lion - peach == maxNum) {
+					for (int i = 10; i >= 0; i--) {
+						if (maxEachArrow[i] < lionEachArrow[i]) {
+							maxEachArrow = Arrays.copyOf(lionEachArrow, lionEachArrow.length);
+						}
+						// 저장된 최대 점수의 최소 점수 갯수가 더 많은 경우 비교 취소
+						else if (maxEachArrow[i] > lionEachArrow[i])
+							break;
+					}
+				}
+				// 마지막 자리이기 때문에 리턴
+				return;
+			}
+
+			// 해당 자리를 이길 수 있을만큼 화살이 존재하고 이기기 위해 화살을 쏠 때
+			if (peachEachArrow[crntPos] < remainArrow) {
+				//해당 자리에 쏜 화살의 수를 기록
+				lionEachArrow[crntPos] = peachEachArrow[crntPos] + 1;
+				//다음 자리로 넘어갈 때 [다음 자리의 번호, 남은 화살 수] 를 인자로 넘겨줌.
+				dfs(crntPos + 1, remainArrow - (peachEachArrow[crntPos] + 1));
+				//방문 끝났으면 자리값 초기화
+				lionEachArrow[crntPos] = 0;
+			}
+
+			// 화살을 쏘지 않을 떄
+			dfs(crntPos + 1, remainArrow);
+		}
+
+		public int[] solution(int n, int[] info) {
+			peachEachArrow = info;
+			int[] answer = { -1 };
+
+			dfs(0, n);
+			//기록된 최대 차이 값이 없다면 -1반환
+			if (maxNum != 0)
+				answer = maxEachArrow;
+
+			return answer;
+		}
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(Arrays.toString(s.solution(5, new int[] { 2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 })));
+	}
+}
