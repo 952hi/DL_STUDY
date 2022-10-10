884197b566bdd25fc22a82f06e821219e972d5ec
이청 <s2econd.blue@gmail.com>
2022-10-10 14:08:33 +0900

    readme.md 복구

---
+++ 2022.09/_0905/ChungLee/PG_추석트래픽.java
@@ -0,0 +1,18 @@
+package _0905.ChungLee;
+
+import java.util.*;
+import java.io.*;
+
+public class PG_추석트래픽 {
+	public static int solution(String[] lines) {
+		
+        System.out.println(lines[0].substring(1));
+        int answer = 0;
+        return answer;
+    }
+	public static void main(String[] args) {
+		String[] list = new String[1];
+		list[0] ="2016-09-15 00:00:00.000 3s";
+		solution(list);
+	}
+}
